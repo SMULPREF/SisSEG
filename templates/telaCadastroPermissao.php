26780c078130c379fa0c8d3795e16ed4edf39677
Gustavo08Mendes <116823674+Gustavo08Mendes@users.noreply.github.com>
2023-08-21 13:33:17 -0300

    Adicionado todos os campos de preenchimento

--- templates/telaCadastroPermissao.php
+++ templates/telaCadastroPermissao.php
@@ -38,38 +38,124 @@
             <div class="row mb-4">
                 
                 <div class="col">
-                    <input type="text" id="form6Example3" class="form-control" />
-                    <label class="form-label" for="form6Example3">Company name</label>
+                    <div class="form-outline">
+                        <input type="text" id="form6Example2" class="form-control" />
+                        <label class="form-label" for="form6Example2">Usuario</label>
+                    </div>
                 </div>
 
                 <div class="col">
-                    <input type="text" id="form6Example4" class="form-control" />
-                    <label class="form-label" for="form6Example4">Address</label>
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>Aprova Digital</label>
                 </div>
 
                 <div class="col">
-                    <input type="email" id="form6Example5" class="form-control" />
-                    <label class="form-label" for="form6Example5">Email</label>
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>Portal de Licenciamento/Anistia</label>
                 </div>
             </div>
 
-            <div class="form-outline mb-4">
-                <input type="number" id="form6Example6" class="form-control" />
-                <label class="form-label" for="form6Example6">Phone</label>
+            <div class="row mb-4">
+                
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>SEI</label>
+                </div>
+
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>SIMPROC</label>
+                </div>
+
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>SISACOE</label>
+                </div>
+
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>SisSEL</label>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>SLCE</label>
+                </div>
+
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>SLCII</label>
+                </div>
+
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                    <label>SITUAÇÃO (Ativo/Inativo/Suspenso)</label>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <div class="col">
+                    <div class="form-outline">
+                        <input type="email" id="form6Example1" class="form-control" />
+                        <label class="form-label" for="form6Example1">E-MAIL</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <input type="text" id="form6Example2" class="form-control" />
+                        <label class="form-label" for="form6Example2">UNIDADE DE TRABALHO</label>
+                    </div>
+                </div>
             </div>
 
             <div class="form-outline mb-4">
                 <textarea class="form-control" id="form6Example7" rows="4"></textarea>
-                <label class="form-label" for="form6Example7">Additional information</label>
-            </div>
-
-            <div class="form-check d-flex justify-content-center mb-4">
-                <input class="form-check-input me-2" type="checkbox" value="" id="form6Example8" checked />
-                <label class="form-check-label" for="form6Example8"> Create an account? </label>
+                <label class="form-label" for="form6Example7">Observações</label>
             </div>
 
             <button type="submit" class="btn btn-primary btn-block mb-4">Enviar</button>
 
+
+            
             </form>
         </div>
     </body>
@@ -86,7 +172,7 @@
     }
 
     .busca-section{
-        background-color: #cfcfcf;
+        background-color: #ebebeb;
         padding: -30px;
     }
 
