eec65d2437f6da723ba839feb3c38cdfb8dbda23
PMSPSMUL <55211643+brunoblv@users.noreply.github.com>
2023-08-22 20:10:53 -0300

    Versão1.0

--- insereUsuarios.php
+++ insereUsuarios.php
@@ -1,80 +1,103 @@
 <?php
 
 if (isset($_POST['executar'])) {
-    
-$ldap_username = "usr_SMDU_Freenas"; // Nome de usuário com privilégios de leitura no AD
-$ldap_password = "Prodam01"; // Senha do usuário com privilégios de leitura no AD
-$base_dn = "ou=Users,ou=SMUL,dc=rede,dc=sp"; // DN da OU que contém os usuários no AD
 
-// Conectar ao servidor LDAP
-$ldap_connection = ldap_connect("ldap://10.10.68.49");
+    $ldap_username = "usr_SMDU_Freenas"; // Nome de usuário com privilégios de leitura no AD
+    $ldap_password = "Prodam01"; // Senha do usuário com privilégios de leitura no AD
+    $base_dn = "ou=Users,ou=SMUL,dc=rede,dc=sp"; // DN da OU que contém os usuários no AD
 
-if ($ldap_connection) {
-    // Autenticar no servidor LDAP
-    $bind = ldap_bind($ldap_connection, $ldap_username, $ldap_password);
+    // Conectar ao servidor LDAP
+    $ldap_connection = ldap_connect("ldap://10.10.68.49");
 
-    if ($bind) {
-        // Pesquisar todos os usuários na base DN especificada
-        $ldap_filter = "(objectClass=user)";
-        $ldap_attributes = array("samaccountname", "cn", "mail");
-        $search = ldap_search($ldap_connection, $base_dn, $ldap_filter, $ldap_attributes);
-        $entries = ldap_get_entries($ldap_connection, $search);
+    if ($ldap_connection) {
+        // Autenticar no servidor LDAP
+        $bind = ldap_bind($ldap_connection, $ldap_username, $ldap_password);
 
-        // Conectar ao banco de dados sisseg
-        $db_host = "localhost";
-        $db_user = "root";
-        $db_password = "";
-        $db_name = "sisseg";
+        if ($bind) {
+            // Pesquisar todos os usuários na base DN especificada
+            $ldap_filter = "(objectClass=user)";
+            $ldap_attributes = array("samaccountname", "cn", "mail");
+            $search = ldap_search($ldap_connection, $base_dn, $ldap_filter, $ldap_attributes);
+            $entries = ldap_get_entries($ldap_connection, $search);
+
+            // Conectar ao banco de dados sisseg
+            $db_host = "localhost";
+            $db_user = "root";
+            $db_password = "";
+            $db_name = "sisseg";
+
+            $db_connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+            if (!$db_connection) {
+                die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
+            }
+
+            // Inserir registros encontrados no banco de dados
+            if ($entries['count'] > 0) {
+                for ($i = 0; $i < $entries['count']; $i++) {
+                    $login = $entries[$i]["samaccountname"][0];
+                    $nome = mysqli_real_escape_string($db_connection, $entries[$i]["cn"][0]);
+                    $email = isset($entries[$i]["mail"][0]) ? $entries[$i]["mail"][0] : "";
         
-        $db_connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
-
-        if (!$db_connection) {
-            die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
-        }
-
-        // Inserir registros encontrados no banco de dados
+                    // Calcular o dígito de controle
+                    $rf = substr($login, 1); // Remover o 'D' do início
+                    $rf_array = str_split($rf);
+                    $soma = 0;
+                    $peso = 7;
         
-        if ($entries['count'] > 0) {
-            for ($i = 0; $i < $entries['count']; $i++) {
-                $login = $entries[$i]["samaccountname"][0];
-                $nome = mysqli_real_escape_string($db_connection, $entries[$i]["cn"][0]);
-                $email = isset($entries[$i]["mail"][0]) ? $entries[$i]["mail"][0] : "";
+                    foreach ($rf_array as $digito) {
+                        $soma += (int)$digito * $peso;
+                        $peso--;
+                    }
+        
+                    $resto = $soma % 11;
+        
+                    if ($resto == 0) {
+                        $digito_controle = 1;
+                    } elseif ($resto == 1) {
+                        $digito_controle = 0;
+                    } else {
+                        $digito_controle = 11 - $resto;
+                    }
+        
+                    // Combinar o dígito de controle com o valor do 'rf'
+                    $rf .= $digito_controle;
+        
+                    // Verificar se o registro já existe
 
-                // Verificar se o registro já existe
-                $check_query = "SELECT * FROM servidores WHERE user = '$login'";
-                $check_result = mysqli_query($db_connection, $check_query);
+                    mysqli_set_charset($db_connection, "utf8");
 
-                if (mysqli_num_rows($check_result) == 0) {
-                    // O registro não existe, então inserir
-                    $insert_query = "INSERT INTO servidores (user, nome, email) VALUES ('$login', '$nome', '$email')";
-                    $insert_result = mysqli_query($db_connection, $insert_query);
+                    $check_query = "SELECT * FROM servidores WHERE user = '$login'";
+                    $check_result = mysqli_query($db_connection, $check_query);
+        
+                    if (mysqli_num_rows($check_result) == 0) {
+                        // O registro não existe, então inserir
+                        $insert_query = "INSERT INTO servidores (user, nome, email, rf) VALUES ('$login', '$nome', '$email', '$rf')";
+                        $insert_result = mysqli_query($db_connection, $insert_query);
 
-                    if (!$insert_result) {
-                        echo "Erro ao inserir registro: " . mysqli_error($db_connection) . "<br>";
+                        if (!$insert_result) {
+                            echo "Erro ao inserir registro: " . mysqli_error($db_connection) . "<br>";
+                        }
                     }
                 }
+            } else {
+                echo "Nenhum usuário encontrado.";
             }
+
+            // Fechar a conexão com o banco de dados
+            mysqli_close($db_connection);
         } else {
-            echo "Nenhum usuário encontrado.";
+            echo "Falha na autenticação no servidor LDAP.";
         }
 
-        // Fechar a conexão com o banco de dados
-        mysqli_close($db_connection);
+        // Fechar a conexão LDAP
+        ldap_close($ldap_connection);
     } else {
-        echo "Falha na autenticação no servidor LDAP.";
+        echo "Falha na conexão com o servidor LDAP.";
     }
-
-    // Fechar a conexão LDAP
-    ldap_close($ldap_connection);
-} else {
-    echo "Falha na conexão com o servidor LDAP.";
-}
-
     echo "O script foi executado com sucesso.";
 
     // Redirecionar de volta para principal.php após a execução do script
     header("Location: principal.php");
     exit; // Certifique-se de que o script não continue após o redirecionamento
 }
-
-?>
